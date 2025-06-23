f53d86f30065af0d21a3e670391c1898f794b5f5
dimasinr <dimas.indra773@gmail.com>
2025-06-24 00:00:33 +0700

    update settings

--- presensidash/settings.py
+++ presensidash/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--%0zjk+($0-jrsd1kdt43(^#xt6ak#l@ekf-&k&i%f)j&khys)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
